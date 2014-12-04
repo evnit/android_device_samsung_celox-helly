94c17d4153c68db893cd500655dfadd3d17cd6e6
evnit <evnithn@gmail.com>
2014-12-04 21:11:27 +0700

    Update Celox Helly ...

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,33 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# inherit from common msm8660
+-include device/samsung/msm8660-common/BoardConfigCommon.mk
+
+## Kernel Toolchain Linaro 4.7.4
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := cortex-a8
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8660_SURF
+
+# Kernel
+BOARD_KERNEL_BASE := 0x48000000
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.selinux=permissive usb_id_pin_rework=true no_console_suspend=true zcache
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
+TARGET_KERNEL_SOURCE := kernel/samsung/msm8660-common
+
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_SUPPRESS_EMMC_WIPE := true
